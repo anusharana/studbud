4eb5396397c75cb4ea0717087fc14fed7a0b28c7
anusharana <58274495+anusharana@users.noreply.github.com>
2022-05-09 16:34:26 +1000

    SCSS amendments

--- dist/index.f3bd186e.js
+++ dist/index.f3bd186e.js
@@ -1,2 +1,8 @@
+//
+let button = document.querySelector('.modal');
+let modal = document.querySelector('.modal');
+button.addEventListener('click', function() {
+    modal.classList.add('is-active');
+});
 
 //# sourceMappingURL=index.f3bd186e.js.map
---
+++ public/script.js
@@ -0,0 +1,7 @@
+//
+let button = document.querySelector('.modal');
+let modal = document.querySelector('.modal');
+
+button.addEventListener('click', function() {
+    modal.classList.add('is-active');
+})
